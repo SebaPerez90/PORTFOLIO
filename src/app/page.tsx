544c4fcaaf92cbedbb3fddd89a202908a86e6425
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2024-02-28 22:58:28 -0300

    footer section design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,25 +40,25 @@ export default function Home() {
 
       <ContactForm />
 
-      <div className='relative gap-40 flex justify-center  items-center bg-white w-full py-8'>
-        <div className='relative w-[20em] h-auto overflow-hidden rounded-md before:animate-[location-animation2_1s_ease-out_alternate-reverse_infinite] before:border-2 before:w-8 before:h-2 before:rounded-[50%] before:absolute before:bottom-[7.5em] before:right-[3.7em] before:z-10 '>
+      <div className='aaaa flex-wrap relative gap-20 flex justify-center items-end  bg-white w-full py-6 pb-10'>
+        {/* <div className=' relative w-[35em] h-[20em]  rounded-lg before:animate-[location-animation2_1s_ease-out_alternate-reverse_infinite] before:w-8 before:h-2 before:rounded-[50%] before:absolute before:bottom-[8.2em] before:right-[3.7em] before:z-10 '>
+          <span>
+            <FaLocationDot className='animate-[location-animation_1s_ease-out_alternate-reverse_infinite] absolute bottom-[4em] right-14 z-20 text-red-500 text-4xl ' />
+          </span>
           <Image
             src={location_image}
             alt='location_image'
             priority
-            className='w-full h-full object-cover z-10 '
+            className='w-full h-full object-cover z-10'
           />
-          <span>
-            <FaLocationDot className='animate-[location-animation_1s_ease-out_alternate-reverse_infinite] absolute bottom-[3.7em] right-14 z-20 text-red-500 text-4xl ' />
-          </span>
-          <address className='mt-4 gap-1 flex items-center justify-center text-sm text-[0.8em] font-semibold text-[#333333ab] '>
+          <address className='gap-1 flex items-center justify-center text-base w-max font-semibold text-[#333333c6] '>
             <FaLocationDot className='text-red-500' />
             calle 1 nº 626, Berazategui, Buenos Aires
           </address>
-        </div>
+        </div> */}
 
         <div className=''>
-          <div>
+          <div className='flex flex-col items-center'>
             <h1 className='font-extrabold text-4xl text-light-400'>
               Seba Perez
             </h1>
@@ -66,12 +66,12 @@ export default function Home() {
               {'< Frontend Developer >'}
             </span>
             <p className='w-[30em] text-[0.8em] font-semibold text-[#3333339e] mt-2'>
-              Agradezco mucho que hayas llegado hasta esta sección del
-              portfolio. Cualquier tipo de feedback es útil para continuar
-              aprendiendo y mejorando.
+              {engLanguageActive
+                ? 'I really appreciate that you have reached this section of the portfolio. Any kind of feedback is useful to continue learning and improving.'
+                : 'Agradezco mucho que hayas llegado hasta esta sección del portfolio. Cualquier tipo de feedback es útil para continuar aprendiendo y mejorando.'}
             </p>
           </div>
-          <ul className='absolute bottom-4 translate-x-6 flex justify-center gap-10 font-semibold text-light-500 opacity-70'>
+          <ul className='flex justify-center gap-8 font-semibold text-light-500 opacity-70'>
             <li>
               <Link
                 href={'https://www.linkedin.com/in/sebaperez90/'}
@@ -79,17 +79,17 @@ export default function Home() {
                 Linkedin
               </Link>
             </li>
-            <li>
+            <li className='border-l-[1px] border-light-500 pl-4'>
               <Link href={'https://github.com/SebaPerez90'} target='_blank'>
                 Github
               </Link>
             </li>
-            <li>
+            <li className='border-l-[1px] border-light-500 pl-4'>
               <Link href={''} target='_blank'>
                 Facebook
               </Link>
             </li>
-            <li>
+            <li className='border-l-[1px] border-light-500 pl-4'>
               <Link href={'#home'}>Home</Link>
             </li>
           </ul>
