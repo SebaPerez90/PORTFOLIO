6c9a4e5f86c713cd81390943cdc875521b44a62c
SebaPerez90 <sebastianalbertoperez90@gmail.com>
2024-03-04 17:32:36 -0300

    appliying media queris to the proyect section

--- src/components/main/Proyects.tsx
+++ src/components/main/Proyects.tsx
@@ -21,7 +21,7 @@ const Proyects = () => {
   return (
     <section
       id='projects-section'
-      className='mt-32 lg:w-[80%] w-[70%] flex flex-col justify-center items-center'>
+      className='mt-32 lg:w-[80%]  md:w-[90%] w-full flex flex-col justify-center items-center'>
       <h1
         className='text-3xl [letter-spacing:15px] text-[#313131]'
         style={roboto.style}>
@@ -30,9 +30,9 @@ const Proyects = () => {
 
       {projects.map((item, index) => (
         <div
-          className='flex-col lg:flex-row flex mt-40 rounded-md shadow-[14px_30px_20px_-10px_#0000004f]'
+          className='flex-col sm:flex-row flex mt-40 rounded-none md:rounded-md shadow-[14px_30px_20px_-10px_#0000004f]'
           key={index}>
-          <article className='rounded-[0.5em_0_0_0.5em] gap-12 flex flex-col justify-around items-center bg-white w-full lg:w-1/2 py-8'>
+          <article className='relative rounded-none md:rounded-[0.5em_0_0_0.5em] gap-12 flex flex-col justify-around items-center bg-white w-full py-8'>
             <h2
               className='text-4xl text-center font-extrabold text-[#454545d3]'
               style={roboto.style}>
@@ -63,9 +63,9 @@ const Proyects = () => {
                 )}
               </div>
             </div>
-            <ul className='flex gap-10 items-center'>
+            <ul className='absolute bottom-[-4.5em] sm:bottom-4 sm:relative flex sm:flex-col-reverse md:flex-row gap-4 lg:gap-10 items-center'>
               <Link
-                className='flex items-center p-[0.8em_2em] gap-2 text-white rounded-full bg-gradient-to-b from-light-900 via-light-900 font-bold to-[#7d27ffae] border-[1px] border-[#4f2194] hover:translate-y-[-0.3em] hover:shadow-[0px_8px_7px_#00000049] duration-200 active:border-[#dedede] hover:bg-[#4f2194] hover:border-light-900 hover:text-[#ffffff93]'
+                className='flex items-center p-[0.8em_3em] md:p-[0.8em_2em] gap-2 text-white rounded-full bg-gradient-to-b from-light-900 via-light-900 font-bold to-[#7d27ffae] border-[1px] border-[#4f2194] hover:translate-y-[-0.3em] hover:shadow-[0px_8px_7px_#00000049] duration-200 active:border-[#dedede] hover:bg-[#4f2194] hover:border-light-900 hover:text-[#ffffff93]'
                 href={item.code}>
                 <FiGithub className='text-xl' />
                 code
@@ -78,7 +78,7 @@ const Proyects = () => {
               </Link>
             </ul>
           </article>
-          <article className='w-full lg:w-1/2 flex justify-center bg-light-700 items-center rounded-[0_0.5em_0.5em_0]'>
+          <article className='pb-8 pt-24 sm:pt-0 sm:pb-0 w-full flex justify-center bg-light-700 items-center rounded-none md:rounded-[0_0.5em_0.5em_0]'>
             <div className='custom-scrollbar flex w-[90%] [scroll-snap-type:x_mandatory] overflow-x-auto web'>
               {item.screen_shoots.map((item, index) => (
                 <Image
