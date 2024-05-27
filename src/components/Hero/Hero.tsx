ffd776f2045b835a8233b7023173527d77ee724d
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-05-27 00:21:22 -0300

    update

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
     <section
       id='hero-section
     '
-      className='max-[520px]:h-auto max-[520px]:pt-12 max-[520px]:flex-col xl:h-auto dark:before:bg-theme_dark-main-bg before:absolute before:left-0 before:top-0 before:w-full before:h-full max-[520px]:bg-gradient-to-t max-[520px]:from-white max-[520px]:to-light-700 max-[520px]:pl-8 relative bg-white flex items-center justify-between pl-16 w-full h-[34em]'>
+      className='max-[520px]:h-auto max-[520px]:pt-12 max-[520px]:flex-col xl:h-auto dark:before:bg-theme_dark-main-bg before:absolute before:left-0 before:top-0 before:w-full before:h-full max-[520px]:bg-gradient-to-t max-[520px]:from-white max-[520px]:to-light-700 max-[520px]:pl-8 relative bg-white flex items-center justify-between pl-16 w-full h-[38em]'>
       <span className='dark:opacity-40 z-10 absolute h-12 w-12 rounded-full bg-[#dcbfff] blur-md [box-shadow:0_0_40px_#dcbfff] duration-300 left-[20%] lg:left-0 top-0 animate-[bouncing2_16s_linear_alternate-reverse_infinite]'></span>
 
       <span className='dark:opacity-40 z-10 absolute h-8 w-8 rounded-full bg-[#c8ffd4] blur-md [box-shadow:0_0_40px_#c8ffd4] duration-300 right-[10em] lg:right-[50%] top-0 animate-[bouncing_16s_linear_alternate-reverse_infinite]'></span>
@@ -44,14 +44,17 @@ const Hero = () => {
           </strong>
           <b className='text-7xl'>.</b>
         </h1>
-        <span className='text-[#333333e3] dark:text-slate-50 font-black italic font-mono'>{'</'}FULLSTACK DEVELOPER{'>'}</span>
+        <span className='text-[#333333e3] dark:text-slate-50 font-black italic '>
+          <b className='text-light-500 font-black text-2xl'>{'</ '}</b>FULLSTACK DEVELOPER
+          <b className='text-light-500 font-black text-2xl'>{' >'}</b>
+        </span>
         <p className=' 2xl:text-lg 2xl:w-full xl:text-base dark:text-slate-200 max-[520px]:text-[#3c3c3ccb] max-[520px]:w-[90%] text-sm font-semibold font-sans text-[#3b3b3b] w-[23em]'>
           {engLanguageActive
             ? `With 2 years of experience in web development, I have a profile that is more oriented towards the Frontend area. My goal is to continue growing as a developer and contribute to the success of an innovative team.`
             : 'Con 2 años de experiencia en el desarrollo web. Tengo un perfil con mayor orientación en el área Frontend. Mi objetivo es seguir creciendo como desarrollador y contribuir al éxito de un equipo innovador.'}
         </p>
 
-        <DownloadBtn />  
+        <DownloadBtn />
       </motion.article>
       <Avatar />
     </section>
--- src/components/Hero/Social.tsx
+++ src/components/Hero/Social.tsx
@@ -10,21 +10,21 @@ const Social = () => {
         href={'https://github.com/SebaPerez90'}
         target='_blank'
         aria-label='social-network-link'
-        className='cursor-pointer relative before:absolute before:w-9 before:h-9 before:bg-black before:-z-10 before:-left-[0.6em] before:-bottom-[0.6em] before:rounded-full text-white'>
+        className='cursor-pointer relative before:absolute before:w-9 before:h-9 before:bg-black before:-z-10 before:-left-[0.6em] before:-bottom-[0.6em] before:rounded-full text-white after:absolute after:left-0 after:bottom-0 after:w-full after:h-full dark:hover:after:bg-[#ffffff2a] hover:after:bg-[#00000023] after:duration-150 after:rounded-full after:scale-[3.5] active:scale-90 after:z-10'>
         <FiGithub />
       </Link>
       <Link
         href={'https://x.com/_SebaPerez_'}
         target='_blank'
         aria-label='social-network-link'
-        className='cursor-pointer relative before:absolute before:w-9 before:h-9 before:bg-black before:-z-10 before:-left-[0.6em] before:-bottom-[0.6em] before:rounded-full text-white'>
+        className='cursor-pointer relative before:absolute before:w-9 before:h-9 before:bg-black before:-z-10 before:-left-[0.6em] before:-bottom-[0.6em] before:rounded-full text-white after:absolute after:left-0 after:bottom-0 after:w-full after:h-full dark:hover:after:bg-[#ffffff2a] hover:after:bg-[#00000023] after:duration-150 after:rounded-full after:scale-[3.5] active:scale-90 after:z-10'>
         <FaXTwitter />
       </Link>
       <Link
         href={'https://www.linkedin.com/in/sebaperez90/'}
         target='_blank'
         aria-label='social-network-link'
-        className='cursor-pointer relative before:absolute before:w-9 before:h-9 before:bg-light-400 before:-z-10 before:-left-[0.6em] before:-bottom-[0.6em] before:rounded-full text-white'>
+        className='cursor-pointer relative before:absolute before:w-9 before:h-9 before:bg-light-400 before:-z-10 before:-left-[0.6em] before:-bottom-[0.6em] before:rounded-full text-white after:absolute after:left-0 after:bottom-0 after:w-full after:h-full dark:hover:after:bg-[#ffffff2a] hover:after:bg-[#00000023] after:duration-150 after:rounded-full after:scale-[3.5] active:scale-90 after:z-10'>
         <FaLinkedinIn />
       </Link>
     </ul>
@@ -33,4 +33,10 @@ const Social = () => {
 
 export default Social;
 
-
+/**<Link
+            target='_blank'
+            href={'https://github.com/SebaPerez90'}
+            aria-label='social-network-btn'
+            className='relative before:absolute before:left-0 before:bottom-0 before:w-full before:h-full dark:hover:before:bg-[#ffffff2a] hover:before:bg-[#00000023] before:duration-150 before:rounded-full before:scale-[1.9] before:-z-10 text-white bg-black text-3xl  rounded-full hover:outline hover:outline-1 hover:outline-white'>
+            <FaGithub />
+          </Link> */
