0931ae8b3221d6f649c0272245eada397a8c8681
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-05-27 00:24:53 -0300

    update

--- src/components/Hero/AvatarHero.tsx
+++ src/components/Hero/AvatarHero.tsx
@@ -8,7 +8,7 @@ const Avatar = () => {
   const { engLanguageActive } = useStore();
 
   return (
-    <div className='max-[520px]:h-[12em] max-[520px]:pb-7 max-[520px]:items-end max-[520px]:justify-start max-[520px]:w-full max-[520px]:right-0 max-[520px]:relative xl:w-[45em] dark:[background:linear-gradient(to_right,#00000000,#00000000)] max-[700px]:absolute max-[700px]:right-36 flex items-end justify-end self-end lg:bg-gradient-to-l from-light-500 to-[#DCBFFF] h-[40em] w-[38em] z-50'>
+    <div className='max-[520px]:h-[12em] max-[520px]:pb-7 max-[520px]:items-end max-[520px]:justify-start max-[520px]:w-full max-[520px]:right-0 max-[520px]:relative xl:w-[45em] dark:[background:linear-gradient(to_right,#00000000,#00000000)] max-[700px]:absolute max-[700px]:right-36 flex items-end justify-end self-end lg:bg-gradient-to-l from-light-500 to-[#DCBFFF] h-[40em] w-[38em] z-40'>
 
       <div className='xl:h-[27em] max-[600px]:[display:none] max-[700px]:w-[17em] max-[700px]:h-[17em] max-[700px]:before:[display:none] w-auto h-[23em] relative max-[820px]:absolute max-[820px]:left-[23em] left-24 lg:left-0 before:absolute before:w-[25em] before:h-[25em] before:rounded-full before:-z-10 z-10 dark:before:bg-theme_dark-sup-pink/40 before:bg-light-200/60 lg:dark:before:bg-white lg:before:bg-white before:left-[-5em] before:bottom-0'>
         <Image
